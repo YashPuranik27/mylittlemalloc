8257201bf0ffbcddba064dd1fd22c0f970bb63ad
Zixinzzzz <146487306+Zixinzzzz@users.noreply.github.com>
2023-10-17 22:49:00 -0400

    Update testmalloc.c

--- testmalloc.c
+++ testmalloc.c
@@ -2,49 +2,45 @@
 #include <string.h>
 #include "mymalloc.h"
 
-int main() {
-    // Test 1: malloc() reserves unallocated memory.
-    void *ptr1 = malloc(16);
-    if (ptr1 == NULL) {
-        printf("Test 1 Failed: malloc() did not reserve memory.\n");
-        return 1;
+// Test 1: malloc() reserves unallocated memory.
+void testMalloc() {
+    int *ptr = (int *)malloc(sizeof(int));
+    if (ptr == NULL) {
+        printf("Test 1 FAILED: malloc() did not allocate memory\n");
+    } else {
+        printf("Test 1 PASSED\n");
+    }
+}
+
+// Test 2: When successful, malloc() returns a pointer to an object that does not overlap with any other allocated object.
+void testMallocOverlap() {
+    int *ptr1 = (int *)malloc(sizeof(int));
+    int *ptr2 = (int *)malloc(sizeof(int));
+
+    if (ptr1 == NULL || ptr2 == NULL) {
+        printf("Test 2 FAILED: malloc() did not allocate memory\n");
+        return;
     }
 
-    // Test 2: When successful, malloc() returns a pointer to an object that does not overlap with any other allocated object.
-    void *ptr2 = malloc(32);
-    if ((char *)ptr1 + 16 > (char *)ptr2) {
-        printf("Test 2 Failed: Allocated objects overlap.\n");
-        return 1;
+    if (ptr2 <= ptr1 + sizeof(int) && ptr2 >= ptr1) {
+        printf("Test 2 FAILED: Overlapping memory regions\n");
+    } else {
+        printf("Test 2 PASSED\n");
     }
 
-    // Test 3: Write a program that allocates several large objects and checks for overlapping.
-    char *pattern = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    for (int i = 0; i < 5; ++i) {
-        void *obj = malloc(20);
-        if (obj == NULL) {
-            printf("Test 3 Failed: malloc() did not reserve memory.\n");
-            return 1;
-        }
-        memcpy(obj, pattern, strlen(pattern));
-        pattern++;
-    }
-
-    // Test 4: free() deallocates memory.
     free(ptr1);
-    void *ptr3 = malloc(16);
-    if (ptr3 != ptr1) {
-        printf("Test 4 Failed: free() did not deallocate memory.\n");
-        return 1;
-    }
-
-    // Test 5: malloc() and free() arrange so that adjacent free blocks are coalesced.
     free(ptr2);
-    void *ptr4 = malloc(48);
-    if (ptr4 != ptr2) {
-        printf("Test 5 Failed: Coalescing of adjacent free blocks did not occur.\n");
-        return 1;
-    }
+}
 
-    printf("All tests passed!\n");
+// Test 3: Write a program that allocates several large objects. Once allocation is complete, it fills each object with a distinct byte pattern.
+void testDistinctBytePattern() {
+    
+}
+
+int main() {
+    testMalloc();
+    testMallocOverlap();
+    testDistinctBytePattern();
+
     return 0;
 }

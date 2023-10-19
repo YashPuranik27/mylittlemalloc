789926ca83e543d0c331b1d71c52b039ee4c2db6
Yash Puranik <puranik.yash@gmail.com>
2023-10-19 18:34:02 -0400

    Added find_memory_block, and free_memory_block.
    *NOTE: NOT FULLY WORKING YET WHERE YOU SEE "TODO:"
    
    Changed coalesce to check whether the header is within memory bounds before merging
    
    Changed my_malloc to where we are no longer coalescing in my_malloc, rather we have a dedicated function free_memory_block. Also cleaned up intitializing new blocks (headers).
    
    find_memory_block traverses each header and tries to find suitable ones. We have 2 cases so far, if there is an exact match found, or if there is unusable space in the block.
    
    Changed my_free slightly. All of the checks are tweaked a bit. Checking if initialized is still a work in progress. Coalescing is now done in free_memory_block.
    
    free_memory_block does the freeing of memory. Comments in the code explain step by step.

--- mymalloc.c
+++ mymalloc.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
+#include "mymalloc.h"
 
 #define MEMLENGTH 512          // Define the length of the memory array.
 #define MEMLENGTH_BYTES (MEMLENGTH * sizeof(double))  // Define the byte size of the memory.
@@ -8,117 +9,179 @@
 #define ROUNDUP(x) (((x) + 7) & ~7)  // Macro for rounding up to the nearest 8.
 #define MAX_BYTE 4096          // Define maximum byte constant for use in the coalesce function.
 
-typedef struct header
-{
-    size_t size;
-    int is_occupied;
-} HEADER;
-
 static double memory[MEMLENGTH];  // Declare a static array of memory.
+void *find_memory_block(int size, int line, char *file, HEADER *current);
+int free_memory_block(void *ptr, int line, char *file);
+
 
 // Function to coalesce (merge) adjacent free memory chunks.
 void coalesce() {
-    HEADER *current = (HEADER *)memory;  // Cast memory array to HEADER type and assign to current.
-    int n_byte = 0;  // Initialize byte counter.
-    // Loop until total traversed bytes plus current size is less than MAX_BYTE.
-    while (n_byte + current->size < MAX_BYTE) {
-        // Find the next header by moving forward by HEADER_SIZE + current's size bytes.
-        HEADER *next = (HEADER *)((char *)current + HEADER_SIZE + current->size);
-        // If both current and next headers are not occupied, merge them.
+    HEADER *current = (HEADER *)memory;
+    HEADER *next;
+
+    // Traverse through the memory blocks
+    // initiates a loop that goes through each memory block (blocks are represented as headers)
+    while ((char *)current < (char *)memory + MEMLENGTH_BYTES) {
+        next = (HEADER *)((char *)current + HEADER_SIZE + current->size);
+
+        // Ensure next header is within memory bounds
+        // Before attempting to merge blocks, this checks whether the next header is within the memory bounds to avoid accessing memory outside of the allocated range
+        if ((char *)next >= (char *)memory + MEMLENGTH_BYTES) break;
+
+        // If the current and next blocks are free, merge them
+        // If both adjacent blocks are not occupied, they are merged into one larger block. This is done by updating the size of the current block to include the size of the next block and the header size
         if (!current->is_occupied && !next->is_occupied) {
-            current->size += HEADER_SIZE + next->size;
+            current->size += next->size + HEADER_SIZE;
+        } else {
+            // Move to the next block
+            // If the blocks are not merged, we move on to the next block to continue the coalescing process
+            current = next;
         }
-        // Increment n_byte by the sum of HEADER_SIZE and current size.
-        n_byte += HEADER_SIZE + current->size;
-        // Move to the next header.
-        current = next;
     }
 }
 
 // Function to allocate memory of given size and log line and file info.
 void *my_malloc(size_t size, int line, char *file) {
-    // Return NULL if size is 0 or if requested size is too large.
-    if (size == 0 || size > (MEMLENGTH * 8) - 8) {
+    // Return NULL for invalid sizes
+    // Immediately returns NULL if the requested size is zero, negative, or larger than the available memory, minus the header size
+    if (size <= 0 || size > MEMLENGTH_BYTES - HEADER_SIZE) {
         return NULL;
     }
-    
-    size = ROUNDUP(size);  // Round up size to nearest 8.
-    HEADER *new = (HEADER *)memory, *next;  // Create header pointers new and next.
+    // Align size to 8 bytes
+    // This makes sure that the size is a multiple of 8 bytes for alignment purposes
+    size = ROUNDUP(size);
 
-    // If the memory has not been initialized.
+    HEADER *new = (HEADER *)memory;
+
+    // Check if the memory is uninitialized
+    // If the memory isn’t initialized (size in the first header is zero), it initializes the memory and sets the necessary fields
     if (new->size == 0) {
-        new->size = size;  // Set the size of new header.
-        new->is_occupied = 1;  // Mark new header as occupied.
-        // If there's enough space for another header.
-        if (size < MEMLENGTH_BYTES - 2 * HEADER_SIZE) {
-            // Initialize next header and adjust its size and occupation status.
-            next = (HEADER *)((char *)new + HEADER_SIZE + size);
-            next->size = MEMLENGTH_BYTES - 2 * HEADER_SIZE - size;
+        new->size = size;
+        new->is_occupied = 1;
+
+        // Initialize the next header if there's enough space left
+        // After allocating space for the new block, this part initializes the next block if there's enough space left for another header and block
+        if (size + HEADER_SIZE * 2 <= MEMLENGTH_BYTES) {
+            HEADER *next = (HEADER *)((char *)new + HEADER_SIZE + size);
+            next->size = MEMLENGTH_BYTES - (size + HEADER_SIZE * 2);
             next->is_occupied = 0;
         }
-        // Return a pointer to the allocated memory.
-        return memory + 1;
-    }
-    coalesce();
-    HEADER *head = (HEADER *)memory;  // Define head pointing at the start of memory.
-    // Define next pointing to the chunk of memory right after head's data.
-    next = (HEADER *)((char *)head + HEADER_SIZE + head->size);
-
-    // Find the next free and sufficiently large header.
-    while (next->is_occupied || next->size < size) {
-        next = (HEADER *)((char *)next + next->size);  // Move to the next header.
+        return (void *)(new + 1);
     }
 
-    // Assign and alter header properties.
-    int old_size = next->size;
-    next->size = size;
-    // If there's sufficient space to split the chunk into two.
-    if (old_size - (HEADER_SIZE + size) >= 2 * HEADER_SIZE) {
-        // Initialize and assign a new header in the residual space.
-        ((HEADER *)((char *)next + HEADER_SIZE + size))->size = old_size - (HEADER_SIZE + size);
-        ((HEADER *)((char *)next + HEADER_SIZE + size))->is_occupied = 0;
+    // Try to find and allocate suitable space
+    // This line of code calls find_memory_block to find a suitable space in the already-initialized memory, marking it as occupied
+    HEADER *result = find_memory_block(size, line, file, new);
+    if (!result) {
+        // If no suitable space is found, we throw an error, showing where in the code the allocation failed
+        printf("ERROR: Not enough space at line %d in file %s\n", line, file);
+        return NULL;
     }
-    next->is_occupied = 1;  // Mark next as occupied.
-    // Return a pointer to the allocated memory.
-    return (char *)next + HEADER_SIZE;
+    return (void *)(result + 1);
+}
+
+// Function to find a suitable memory block
+void *find_memory_block(int size, int line, char *file, HEADER *current) {
+    // Minimum space to create a new block
+    // Defines the smallest space that should remain in a block after allocation to create a new block
+    const int MIN_SPACE = 16;
+    HEADER *next;
+
+    // Traversing through the memory blocks
+    // Iterates through each memory block to find a suitable one for allocation
+    while ((char *)current < (char *)memory + MEMLENGTH_BYTES) {
+        next = (HEADER *)((char *)current + HEADER_SIZE + current->size);
+
+        if (!current->is_occupied) {
+            // Case: Exact size match
+            // If a block is unoccupied and matches the requested size exactly, it’s marked as occupied and returned
+            if (current->size == size) {
+                current->is_occupied = 1;
+                return (void *)(current + 1);
+            }
+
+            // Case: Block is larger than required, and space remaining is usable
+            // If a block is larger than required and the space remaining after allocation is enough to host another block, the block is split, and the suitable part is returned
+            if (current->size - size >= MIN_SPACE + HEADER_SIZE) {
+                HEADER *new_next = (HEADER *)((char *)current + HEADER_SIZE + size);
+                new_next->size = current->size - size - HEADER_SIZE;
+                new_next->is_occupied = 0;
+
+                current->size = size;
+                current->is_occupied = 1;
+                return (void *)(current + 1);
+            }
+        }
+
+        // Move to the next memory block
+        // Continues the iteration to the next memory block for evaluation
+        current = next;
+    }
+
+    // If no suitable block is found, return NULL
+    // If traversing finishes without finding a suitable block, the function returns NULL, indicating a failure to allocate memory
+    return NULL;
 }
 
 // Function to free allocated memory and log line and file info.
 void my_free(void *ptr, int line, char *file) {
-    // If memory is not initialized, print an error message and return.
-    if (!((HEADER *)memory)->size) {
-        printf("ERROR: free() cannot be called before memory is initialized \n ORIGIN: at line %d in file %s", line, file);
+    // Check if memory is initialized
+    // This checks if the memory has been initialized before proceeding with the free operation
+    // TODO: Currently a work in progress
+    if ((HEADER *)memory == 0) {
+        printf("ERROR: free() cannot be called before memory is initialized \n ORIGIN: From line %d in file %s", line, file);
         return;
     }
 
-    int in_array = 0;  // Flag variable to check if ptr is in the memory array.
-    // Check if ptr points within the memory array.
-    for (int i = 0; i < MEMLENGTH && !in_array; i++) {
-        in_array = ((char *)(memory) + i == ptr);
-    }
-
-    // If ptr is not in array, print an error message and return.
-    if (!in_array) {
-        printf("ERROR: Pointer is outside the memory \n ORIGIN: at line %d in file %s", line, file);
+    // Check if the pointer is within the memory range
+    // Validates whether the pointer to be freed lies within the bounds of the allocated memory. It simplifies the previous loop by directly checking the pointer against the memory boundaries
+    char *start = (char *)memory;
+    char *end = start + MEMLENGTH_BYTES;
+    if ((char *)ptr < start || (char *)ptr >= end) {
+        printf("ERROR: Pointer is outside the memory \n ORIGIN: From line %d in file %s", line, file);
         return;
     }
 
-    HEADER *current = (HEADER *)memory;  // Define current header pointing at memory start.
-    int current_byte = 0;  // Initialize a counter for traversed bytes.
-    // Loop to find and free the memory chunk to which ptr points.
-    do {
-        void *data_ptr = (void *)(current + 1);  // Define pointer to current's data.
-        // If ptr points to current's data.
-        if (data_ptr == ptr) {
-            // Mark the header as free or print an error message if it's already free.
-            current->is_occupied = (current->is_occupied) ? 0 : printf("ERROR: Pointer is already freed \n ORIGIN: at line %d in file %s", line, file);
-            coalesce();  // Merge adjacent free chunks.
-            return;
-        }
-        // Update the byte counter and move to the next header.
-        current_byte += HEADER_SIZE + current->size;
-        current = (HEADER *)((char *)current + HEADER_SIZE + current->size);
-    } while (current_byte < MEMLENGTH);  // Continue until all memory is traversed.
-    // If ptr does not point to the start of a memory segment, print an error message and return.
-    printf("ERROR: Pointer does not point to the beginning of a memory segment \n ORIGIN: at line %d in file %s", line, file);
+    // Attempt to free the pointer and handle errors
+    // Calls the free_memory_block function to attempt freeing the memory, and handles errors if the memory couldn’t be freed, emitting an error message
+    int successful_free = free_memory_block(ptr, line, file);
+    if (!successful_free) {
+        printf("ERROR: Pointer does not point to the beginning of a memory segment \n ORIGIN: From line %d in file %s", line, file);
+    }
 }
+
+// Function to free a memory block
+int free_memory_block(void *ptr, int line, char *file) {
+    HEADER *current = (HEADER *)memory;
+
+    // Traverse through the memory blocks
+    // Looping through each memory block to find the matching pointer
+    while ((void *)current < (void *)memory + MEMLENGTH_BYTES) {
+        void *data_ptr = (void *)((char *)current + HEADER_SIZE);
+
+        // Check if the pointer matches and is already freed
+        // Verifies if a matching pointer is found and it’s already marked as free, an error message is printed and the memory blocks are coalesced
+        if (current->is_occupied == 0 && data_ptr == ptr) {
+            printf("ERROR: Pointer is already freed \n From line %d in file %s", line, file);
+            coalesce();
+            return 0;
+        }
+
+            // Check if the pointer matches and is not freed, then free it
+            // If a matching pointer that is not freed is found, it gets freed and the memory blocks are coalesced
+        else if (current->is_occupied == 1 && data_ptr == ptr) {
+            current->is_occupied = 0;
+            coalesce();
+            return 1;
+        }
+
+        // Move to the next memory block
+        // Advances to the next memory block for further checking
+        current = (HEADER *)((char *)current + HEADER_SIZE + current->size);
+    }
+
+    // Pointer not found in the memory blocks
+    // Returns -1 if no matching pointer is found after traversing all memory blocks
+    return -1;
+}
+
--- mymalloc.h
+++ mymalloc.h
@@ -1,11 +1,15 @@
 #include <stddef.h>
-#include <mymalloc.c>
+#include "mymalloc.c"
 // MACROS
 #define malloc(size) my_malloc(size, __LINE__, __FILE__)
 #define free(ptr) my_free(ptr, __LINE__, __FILE__)
 
 // STRUCTURES
-
+typedef struct header
+{
+    size_t size;
+    int is_occupied;
+} HEADER;
 
 // FUNCTIONS
 void *my_malloc(size_t size, int line, char *file);

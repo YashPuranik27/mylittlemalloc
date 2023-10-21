216624f3353a27320e47dbbbb32f78775ddb7995
Zixinzzzz <146487306+Zixinzzzz@users.noreply.github.com>
2023-10-20 23:09:32 -0400

    Update memgrind.c

--- memgrind.c
+++ memgrind.c
@@ -9,7 +9,7 @@
 // Function to perform task 1: malloc() and immediately free() a 1-byte object, 120 times.
 void task1() {
     struct timeval start, end;
-    double elapsed_time;
+    double time;
 
     gettimeofday(&start, NULL);
 
@@ -20,15 +20,15 @@ void task1() {
 
     gettimeofday(&end, NULL);
 
-    elapsed_time = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+    time = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
 
-    printf("Task 1: Average time per operation = %f seconds\n", elapsed_time / OBJECT_COUNT);
+    printf("Task 1: Average time per operation = %f microseconds\n", time / OBJECT_COUNT);
 }
 
 // Function to perform task 2: Use malloc() to get 120 1-byte objects, storing the pointers in an array, then use free() to deallocate the chunks.
 void task2() {
     struct timeval start, end;
-    double elapsed_time;
+    double time;
 
     gettimeofday(&start, NULL);
 
@@ -44,15 +44,15 @@ void task2() {
 
     gettimeofday(&end, NULL);
 
-    elapsed_time = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+    time = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
 
-    printf("Task 2: Average time per operation = %f seconds\n", elapsed_time / OBJECT_COUNT);
+    printf("Task 2: Average time per operation = %f microseconds\n", time / OBJECT_COUNT);
 }
 
 // Function to perform task 3: Create an array of 120 pointers. Repeatedly make a random choice between allocating a 1-byte object and adding the pointer to the array and deallocating a previously allocated object (if any), until you have allocated 120 times. Deallocate any remaining objects.
 void task3() {
     struct timeval start, end;
-    double elapsed_time;
+    double time;
 
     gettimeofday(&start, NULL);
 
@@ -77,16 +77,69 @@ void task3() {
 
     gettimeofday(&end, NULL);
 
-    elapsed_time = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+    time = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
 
-    printf("Task 3: Average time per operation = %f seconds\n", elapsed_time / OBJECT_COUNT);
+    printf("Task 3: Average time per operation = %f microseconds\n", time / OBJECT_COUNT);
 }
+
+// Stress Test 4: Allocate and deallocate alternating small and large chunks
+void task4() {
+    struct timeval start, end;
+    double time;
+
+    gettimeofday(&start, NULL);
+
+    for (int i = 0; i < OBJECT_COUNT; i++) {
+        if (i % 2 == 0) {
+            // Allocate a small chunk
+            char *ptr = (char *)malloc(1);
+            free(ptr);
+        } else {
+            // Allocate a large chunk
+            char *ptr = (char *)malloc(1024);
+            free(ptr);
+        }
+    }
+
+    gettimeofday(&end, NULL);
+
+    time = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
+
+    printf("Task 4: Average time per operation = %f microseconds\n", time / OBJECT_COUNT);
+}
+
+// Stress Test 5: Allocate and deallocate randomly sized chunks
+void task5() {
+    struct timeval start, end;
+    double time;
+
+    gettimeofday(&start, NULL);
+
+    // Stress Test 5: Allocate and deallocate randomly sized chunks
+    for (int i = 0; i < OBJECT_COUNT; i++) {
+        // Generate a random size between 1 and 1024
+        int size = rand() % 512 + 1;
+
+        // Allocate and deallocate memory
+        char *ptr = (char *)malloc(size);
+        free(ptr);
+    }
+
+    gettimeofday(&end, NULL);
+
+    time = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
+
+    printf("Task 5: Average time per operation = %f microseconds\n", time / OBJECT_COUNT);
+}
+
 
 int main() {
     for (int i = 0; i < TASK_COUNT; i++) {
         task1();
         task2();
         task3();
+        task4();
+        task5();
     }
     return 0;
 }

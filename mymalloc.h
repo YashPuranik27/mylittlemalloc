32e875bacb2921220334bc0023f3bae34fe64dd9
Zixinzzzz <146487306+Zixinzzzz@users.noreply.github.com>
2023-11-29 00:40:47 -0500

    Update mymalloc.h

--- mymalloc.h
+++ mymalloc.h
@@ -1,11 +1,7 @@
-#include <stddef.h>
-#include "mymalloc.c"
-// MACROS
-#define malloc(size) my_malloc(size, __LINE__, __FILE__)
-#define free(ptr) my_free(ptr, __LINE__, __FILE__)
-
-// STRUCTURES
-
 // FUNCTIONS
 void *my_malloc(size_t size, int line, char *file);
 void my_free(void *ptr, int line, char *file);
+
+// MACROS
+#define malloc(size) my_malloc(size, __LINE__, __FILE__)
+#define free(ptr) my_free(ptr, __LINE__, __FILE__)
